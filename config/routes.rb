102cc290f85e071e2974bd710f4fee9517eb6bc2
Nadia Zhuk <nadia.zhuk@netguru.co>
2019-02-12 16:32:19 +0100

    Customize Devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,13 @@ Rails.application.routes.draw do
   root 'ideas#index'
   devise_for :users, skip: [:registrations]
 
+  devise_scope :user do
+    get 'sign_in', to: 'devise/sessions#new'
+    delete 'sign_out', to: 'devise/sessions#destroy'
+  end
+
   namespace :editor do
     root 'application#index'
-
     resources :users do
       member do
         patch :archive
